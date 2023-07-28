31991c60339efa2f8ad9045405a9ca6d8399a8ef
ChakravartiRaghavan <rchak1@aol.com>
2023-07-28 00:51:36 -0700

    lucid

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/* @type {import('next').NextConfig} */
+/* const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = nextConfig */
+
+
+const nextConfig = {
+  
+    experimental: {
+      outputStandalone: true,
+    },
+    reactStrictMode: true,
+    webpack: (config) => {
+      config.experiments = { 
+        asyncWebAssembly: true,
+        topLevelAwait: true,
+        layers: true
+      }
+      return config
+    }
+  }
+  
+  module.exports = nextConfig
