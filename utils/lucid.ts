1946dd2223f2eab0838a0a5eb0f30237caffb865
ChakravartiRaghavan <rchak1@aol.com>
2023-08-08 16:05:43 -0700

    apiBlockFrost

--- utils/lucid.ts
+++ utils/lucid.ts
@@ -10,16 +10,19 @@ const initLucid = async (wallet: string) => {
     const nami = await window.cardano.nami.enable();
     // const eternl = window.cardano.eternl.enable();
     // const gero = await window.cardano.gero.enable();
+
+    const apiValue = await fetchBlockFrostApi();   
     
     const lucid = await Lucid.new(
-        new Blockfrost('https://cardano-preprod.blockfrost.io/api/v0', "preprodLT9nUTb22P26FiVH42jSFJU2IZaNxZVz") //process.env.NEXT_PUBLIC_BLOCKFROST as string),
+        new Blockfrost('https://cardano-preprod.blockfrost.io/api/v0', apiValue.toString()) //process.env.NEXT_PUBLIC_BLOCKFROST as string),
         ,'Preprod');
+        // ,'Preview');
     // const lucid = await Lucid.new(
-    //         new Blockfrost("https://cardano-preview.blockfrost.io/api/v0", "previewY7wWn4mtcYHascUO7PyxeCXadkAkBVz2"),
+    //         new Blockfrost("https://cardano-preview.blockfrost.io/api/v0", "previewxxxxx"),
     //         "Preview",
     //       );
     // const lucid = await Lucid.new(
-    //         new Blockfrost('https://cardano-mainnet.blockfrost.io/api/v0', "mainneto2wd71NAi5sZMWDHUTXxgvMTEC6ciS2I") //process.env.NEXT_PUBLIC_BLOCKFROST as string),
+    //         new Blockfrost('https://cardano-mainnet.blockfrost.io/api/v0', "mainnetxxxxxx") //process.env.NEXT_PUBLIC_BLOCKFROST as string),
     //         ,'Mainnet')
     //lucid.selectWallet(api)
     lucid.selectWallet(nami)
@@ -28,5 +31,20 @@ const initLucid = async (wallet: string) => {
     return lucid;
 }
 
+
+const fetchBlockFrostApi = async () => {
+    try {
+      const response = await fetch('/api/getData');
+      const data = await response.json();
+      if (data.seed[0]) {
+        const blockfrostApi = data.seed[2].description;
+        console.log("api = ", blockfrostApi)
+        return blockfrostApi; // Return the value
+      }      
+    } catch (error) {
+      console.error('Error fetching secret data:', error);
+    }
+    return null; // Return null if there was an error or no data was found
+  };  
 
 export default initLucid;
